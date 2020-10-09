cd83d9793444ede1eb56ba47147e24fb70345c9c
cojarepublic <60820059+cojarepublic@users.noreply.github.com>
2020-10-09 15:56:51 +0100

    edited notes screen

--- lib/views.dart/notes_list.dart
+++ lib/views.dart/notes_list.dart
@@ -1,6 +1,29 @@
+import 'package:api_test/models/note_for_listing.dart';
 import 'package:flutter/material.dart';
 
 class NotesList extends StatelessWidget {
+
+final List notes = [
+  NoteForListing(
+    noteID: '1',
+    createDateTime: DateTime.now(),
+    lastEditDateTime: DateTime.now(),
+    noteTitle: 'Note 1',
+  ),
+  NoteForListing(
+    noteID: '2',
+    createDateTime: DateTime.now(),
+    lastEditDateTime: DateTime.now(),
+    noteTitle: 'Note 2',
+  ),
+  NoteForListing(
+    noteID: '3',
+    createDateTime: DateTime.now(),
+    lastEditDateTime: DateTime.now(),
+    noteTitle: 'Note 3',
+  ),
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +34,16 @@ class NotesList extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
-      body: Container(),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(height: 1, color: Colors.green,),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Hello'),
+            subtitle: Text('Last edited on 9/10/2020'),
+          );
+        } ,
+        itemCount: 30,
+      ),
     );
   }
 }
